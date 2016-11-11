919c4b13f24616ada27839c52687730aa5ae7f6e
ruizjme <hello@ruizjaime.com>
2016-11-11 13:54:08 +1100

    this

--- core/config.php
+++ core/config.php
@@ -2,10 +2,10 @@
 /*Configuration Settings*/
 
 /*MySQL Settings*/
-define('DB_NAME', 'hw'); //for live website: a8623247_hw
-define('DB_USER', 'root'); //for live website: a8623247_jr
-define('DB_PASSWORD', 'root'); //for live website: 1Abc2-34
-define('DB_HOST', 'localhost:8889'); //for live website: mysql14.000webhost.com
+define('DB_NAME', 'hw');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost:8889');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
