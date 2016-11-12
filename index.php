ad2322a0093aa71b601a7322cf5597f876512ba7
ruizjme <hello@ruizjaime.com>
2016-11-12 20:30:41 +1100

    SQL up and running locally

--- index.php
+++ index.php
@@ -12,24 +12,23 @@ require_once(dirname(__FILE__) . '/core/functions.php');
 ?>
 
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'hw';
-$host = 'localhost';
-$port = 8889;
+$mysqli = mysqli_init();
+if (!$mysqli) {
+    die('mysqli_init failed');
+}
 
-$link = mysqli_init();
-$success = mysqli_real_connect(
-   $link, 
-   $host, 
-   $user, 
-   $password, 
-   $db,
-   $port
-);
+if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+    die('Setting MYSQLI_INIT_COMMAND failed');
+}
 
-$q_sql = "SELECT * FROM t_questions";
-$to_sql = "SELECT * FROM t_topics";
+if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+}
+
+if (!$mysqli->real_connect('localhost', 'root', 'root', 'hw')) {
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
 
 ?>
 
@@ -51,41 +50,47 @@ include 'include/hlight.php';
 ?>
 
 	<?php
-	$questions = mysqli_query($q_sql);
-	$topics = mysqli_query($to_sql);
+
+	$q_sql = "SELECT * FROM t_questions";
+	$to_sql = "SELECT * FROM t_topics";
+
+
+	$questions = mysqli_query($mysqli, $q_sql);
+	$topics = mysqli_query($mysqli, $to_sql);
 	$topic = mysqli_fetch_array($topics);
 	$n = 0;
 
-	echo $questions;
-
-	// while($question = mysqli_fetch_array( $questions )){
-	// 	echo "
-	// 	<div class='form'>
-	// 		<div class='form-top'>
-	// 			<p class='subject'>".$topic['to_subject']."</p>
-	// 			<p class='topic'>".$topic['to_topic']."</p>
-	// 		</div>
-	// 		<div class='form-top-right'>
-	// 			<a href='' data-title='".$result['source']."' data-content='Tags: ".$result['tags']."' data-placement='right'>
-	// 				<i class='material-icons'>more_vert</i>
-	// 			</a>
-	// 		</div>
-	// 		<div class='question'>";
-	// 		if ($question['figure']) {
-	// 		echo "
-	// 		<div class='figure'>
-	// 			<img src='images/".$question['figure']."' />
-	// 		</div>";
-	// 		}
-	// 	echo "	
-	// 			<p>".$question['question']."</p>
-	// 		</div>";
+	while($question = mysqli_fetch_array( $questions )){
+		echo "
+		<div class='form'>
+			<div class='form-top'>
+				<p class='subject'>".$topic['to_subject']."</p>
+				<p class='topic'>".$topic['to_topic']."</p>
+			</div>
+			<div class='form-top-right'>
+				<a href='' data-title='".$result['source']."' data-content='Tags: ".$result['tags']."' data-placement='right'>
+					<i class='material-icons'>more_vert</i>
+				</a>
+			</div>
+			<div class='question'>";
+			if ($question['figure']) {
+			echo "
+			<div class='figure'>
+				<img src='images/".$question['figure']."' />
+			</div>";
+			}
+		echo "	
+				<p>".$question['question']."</p>
+			</div>";
 			
-	// 	echo "
-	// 		<div class='toggle'>".$question['answer']."</div>
-	// 	</div>";
-	// }
-	?>
+		echo "
+			<div class='toggle'>".$question['answer']."</div>
+		</div>";
+	}
+
+
+$mysqli->close();
+?>
 
 </div>
 <?php include 'include/footer.php' ?>
